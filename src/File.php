39bebf9361a62e3a6ef470c1e2443d4ba63e8e27
José Hélio Veríssimoo Jr <jose.helio@gmail.com>
2023-05-07 16:48:01 -0300

    env: settings relatives to php snipher

--- src/File.php
+++ src/File.php
@@ -10,9 +10,7 @@ use Plugse\FP\Exceptions\FileCanNotBeRead;
 
 class File
 {
-
-    const BROKE_LINE = "\n";
-
+    private const BROKE_LINE = "\n";
     public static function readJsonFile(string $filename): array
     {
         return json_decode(self::readFile($filename), JSON_PRETTY_PRINT);
@@ -22,62 +20,53 @@ class File
     {
         $response = [];
         $separator = "=";
-
-        foreach(explode(self::BROKE_LINE, self::readFile($filename)) as $row){
-			if(strlen($row)>0 AND strstr($row, $separator)){
-				$brk = explode($separator, $row);
-				$key = trim($brk[0]);
-				$value = trim($brk[1]);
-				$response[$key] = $value;
-			}
+        foreach (explode(self::BROKE_LINE, self::readFile($filename)) as $row) {
+            if (strlen($row) > 0 and strstr($row, $separator)) {
+                $brk = explode($separator, $row);
+                $key = trim($brk[0]);
+                $value = trim($brk[1]);
+                $response[$key] = $value;
+            }
         }
 
         return $response;
     }
-    
+
     public static function readFile(string $filename): string
     {
-        
-        if(!file_exists($filename)){
+
+        if (!file_exists($filename)) {
             throw new FileNotFound($filename);
         }
 
         $fileContent = file($filename);
-        if(!$fileContent){
+        if (!$fileContent) {
             throw new FileCanNotBeRead($filename);
         }
 
         return implode(self::BROKE_LINE, $fileContent);
-    }    
+    }
 
     public static function saveOnJsonFile(string $filename, array $dataStructure): void
     {
-        self::saveFile(
-            $filename,
-            json_encode($dataStructure, JSON_PRETTY_PRINT),
-            true
-        );
+        self::saveFile($filename, json_encode($dataStructure, JSON_PRETTY_PRINT), true);
     }
 
-	public static function saveOnDotEnvFile(string $filename, array $dataStructure): void
-	{
+    public static function saveOnDotEnvFile(string $filename, array $dataStructure): void
+    {
         $content = '';
-        foreach($dataStructure as $key=>$value){
+        foreach ($dataStructure as $key => $value) {
             $content .= "{$key}={$value}\n";
         }
 
-        self::saveFile(
-            $filename,
-            $content,
-            true
-        );
-	}
+        self::saveFile($filename, $content, true);
+    }
 
-    public static function saveFile(string $filename, string $content, bool $update=false): void
+    public static function saveFile(string $filename, string $content, bool $update = false): void
     {
         self::createPathIfNotExists(dirname($filename));
-        if(!$update){
-            if(file_exists($filename)){
+        if (!$update) {
+            if (file_exists($filename)) {
                 throw new FileAlreadyExists($filename);
             }
         }
@@ -86,30 +75,28 @@ class File
             throw new FileCannotBeOppend($filename);
         }
 
-        if (fwrite($handle, $content) === FALSE) {
+        if (fwrite($handle, $content) === false) {
             throw new FileCannotBeWritten($filename);
         }
 
         fclose($handle);
-
         $action = $update ? 'atualizado' : 'criado';
     }
 
     public static function createPathIfNotExists(string $path): void
     {
-        if(file_exists($path)){
+        if (file_exists($path)) {
             return;
         }
-        
+
         $dir = '';
-        foreach(explode('/', $path) as $dirPiece){
-            if($dirPiece !== '.'){
+        foreach (explode('/', $path) as $dirPiece) {
+            if ($dirPiece !== '.') {
                 $dir .= "/{$dirPiece}";
-                if(!file_exists($dir)){
+                if (!file_exists($dir)) {
                     mkdir($path, 0777, true);
                 }
             }
         }
     }
-
 }
